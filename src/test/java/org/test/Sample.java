32f3e21ea7e9f2d34578c4077265bd1480c4e432
Dhilip018 <brendondhilip42@gmail.com>
2022-09-06 14:16:35 -0400

    vk's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -4,9 +4,9 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		
-		
-		
-		
+		System.out.println("vk 1");
+		System.out.println("vk 4");
+		System.out.println("vk 5");
 		
 		
 		
